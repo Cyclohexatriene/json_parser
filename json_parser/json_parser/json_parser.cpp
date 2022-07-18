a84f996cd1879afed367e84101d0e15a5f0ff2e5
dzcdzcdzc <3200104203@zju.edu.cn>
2022-07-18 22:11:25 +0800

    实现了解析功能，基本完成项目

--- json_parser/json_parser/json_parser.cpp
+++ json_parser/json_parser/json_parser.cpp
@@ -15,6 +15,8 @@ using namespace std;
 #define LIST 6
 
 
+stringstream ss;
+
 class Object {
 private:
     int INT_VAL;
@@ -23,7 +25,8 @@ private:
     bool BOOL_VAL;
     unordered_map<string, Object> OBJECT_VAL;
     vector<Object> LIST_VAL;
-    
+    vector<string> keys;
+
     int type;
     
     void del() {
@@ -32,8 +35,147 @@ private:
         if (type == LIST) LIST_VAL.clear();
         type = NULL;
     }
-    
-    stringstream ss;
+    void set_type(int a) {
+        type = a;
+    }
+
+    void insert(string key, Object val) {
+        if (type != OBJECT) {
+            del();
+            set_type(OBJECT);
+        }
+        keys.push_back(key);
+        OBJECT_VAL[key] = val;
+    }
+    void push(Object val) {
+        if (type != LIST) {
+            del();
+            set_type(LIST);
+        }
+        LIST_VAL.push_back(val);
+    }
+
+    //parsers
+    Object parse_val() {
+        if (ss.peek() == -1) {
+            Object nl;
+            nl.set_type(NULL);
+        }
+        else if (ss.peek() == '"') return parse_str();
+        else if (ss.peek() == 'f' || ss.peek() == 't') return parse_bool();
+        else if (ss.peek() == '{') return parse_object();
+        else if (ss.peek() == '[') return parse_list();
+        else if (ss.peek() == 'n') return parse_null();
+        else return parse_num();
+    }
+    Object parse_num() {
+        string str;
+        bool isfloat = false;
+        while (isdigit(ss.peek()) || ss.peek() == '-' || ss.peek() == '+' || ss.peek() == '.' || ss.peek() == 'e') {
+            if (ss.peek() == '.' || ss.peek() == 'e') isfloat = true;
+            str += ss.get();
+        }
+        if (isfloat) return Object(stof(str));
+        else return Object(stoi(str));
+
+    }
+    Object parse_str() {
+        ss.get();
+        string str;
+        while (ss.peek() != '"') str += ss.get();
+        ss.get();
+        return Object(str);
+    }
+    Object parse_bool() {
+        if (ss.peek() == 'f') {
+            ss.get(); ss.get(); ss.get(); ss.get(); ss.get();
+            return Object(false);
+        }
+        else {
+            ss.get(); ss.get(); ss.get(); ss.get();
+            return Object(true);
+        }
+    }
+    Object parse_object() {
+        Object res;
+        ss.get();
+        while (ss.peek() != '}') {
+            ss >> ws;
+            string key;
+            while (ss.peek() != ' ' && ss.peek() != ':' && ss.peek() != '\t' && ss.peek() != '\n') key += ss.get();
+            ss >> ws; ss.get(); ss >> ws;//remove ':' and whitespaces
+            Object val;
+            val = parse_val();
+            res.insert(key, val);
+            //remove ',' and whitespaces
+            ss >> ws;
+            if (ss.peek() == ',') {
+                ss.get();
+                ss >> ws;
+            }
+        }
+        ss.get();
+        return res;
+    }
+    Object parse_list() {
+        ss.get();
+        Object res;
+        while (ss.peek() != ']') {
+            Object temp = parse_val();
+            res.push(temp);
+            ss >> ws;
+            if (ss.peek() == ',') {
+                ss.get();
+                ss >> ws;
+            }
+        }
+        ss.get();
+        return res;
+        
+    }
+    Object parse_null() {
+        ss.get(); ss.get(); ss.get(); ss.get();
+        return Object();
+    }
+
+    void printNULL() {
+        cout << "null";
+    }
+    void printINT() {
+        cout << INT_VAL;
+    }
+    void printSTR() {
+        cout << STRING_VAL;
+    }
+    void printDOUBLE() {
+        cout << DOUBLE_VAL;
+    }
+    void printBOOL() {
+        if (BOOL_VAL) cout << "true";
+        else cout << "false";
+    }
+    void printOBJECT() {
+        cout << "{ ";
+        bool a = true;
+        for (auto i : keys) {
+            if (a) a = false;
+            else cout << " , ";
+            cout << i;
+            cout << " : ";
+            OBJECT_VAL[i].show(false);
+        }
+        cout << " }";
+    }
+    void printLIST() {
+        cout << "[";
+        bool a = true;
+        for (auto i : LIST_VAL) {
+            if (a) a = false;
+            else cout << " , ";
+            i.show(false);
+        }
+        cout << "]";
+    }
 public:
     //constructors
     Object() {
@@ -72,6 +214,7 @@ public:
         BOOL_VAL = x;
     }
 
+    
     string get_type() {
         if (type == INT) return "INT";
         if (type == DOUBLE) return "DOUBLE";
@@ -81,12 +224,24 @@ public:
         if (type == LIST) return "LIST";
         return "NULL";
     }
-    void set_type(int a) {
-        type = a;
-    }
+    
     void parse(string json) {
-        
+        ss.clear();
+        ss << json;
+        ss >> ws;
+        *this = parse_val();
     }
+    void show(bool end = true) {
+        if (type == NULL) printNULL();
+        else if (type == INT) printINT();
+        else if (type == DOUBLE) printDOUBLE();
+        else if (type == STR) printSTR();
+        else if (type == BOOL) printBOOL();
+        else if (type == OBJECT) printOBJECT();
+        else if (type == LIST) printLIST();
+        if (end) cout << endl;
+    }
+
 
     
 };
@@ -95,5 +250,11 @@ public:
 
 int main()
 {
+    Object test;
+    test.parse(
+        "{ \"headers\": {\"SalesPerson\": [\"sales_id\", \"name\", \"salary\", \"commission_rate\",\"hire_date\"], \"Company\": [\"com_id\", \"name\",\"city\"],\"Orders\":[\"order_id\",\"order_date\",\"com_id\",\"sales_id\",\"amount\"]}, \"rows\": {\"SalesPerson\": [[1, \"John\", 100000, 6, \"4/1/2006\"], [2, \"Amy\", 12000, 5,\"5/1/2010\"], [3, \"Mark\", 65000, 12, \"12/25/2008\"], [4, \"Pam\", 25000, 25,\"1/1/2005\"],[5,\"Alex\",5000,10,\"2/3/2007\"]], \"Company\": [[1, \"RED\",\"Boston\"], [2, \"ORANGE\", \"New York\"],[3, \"YELLOW\", \"Boston\"],[4, \"GREEN\", \"Austin\"]],\"Orders\":[[1,\"1/1/2014\",3,4,10000],[2, \"2/1/2014\", 4, 5, 5000],[3, \"3/1/2014\", 1, 1, 50000],[4, \"4/1/2014\", 1, 4, 25000]]}}"
+    );
+    test.show();
+
     
 }
